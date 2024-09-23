be2b12d7cbe3dc6490b99317113c0dfa71111296
Muhammad Abdulrazek <eng.muhammadabdulrazek@gmail.com>
2024-09-23 20:27:37 +0300

    first commit

---
+++ Modules/Users/database/seeders/UsersDatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Users\database\seeders;
+
+use Illuminate\Database\Seeder;
+use Modules\Users\app\Models\User;
+
+class UsersDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+    }
+}
